23e14f58a607de8d7260c01750eac030e9645391
whatthefoobar <irina.mntn@gmail.com>
2024-02-15 15:09:55 +0100

    iterrating through the channels works

--- src/player/Player.jsx
+++ src/player/Player.jsx
@@ -7,6 +7,7 @@ const Player = () => {
   const { channels, loading, error } = useRadioChannels();
   console.log("all channels", channels);
   const [audioState, setAudioState] = useState("stopped");
+  const [currentChannelIndex, setCurrentChannelIndex] = useState(0);
   const audioRef = useRef(null);
 
   // Handler to start playback
@@ -22,25 +23,54 @@ const Player = () => {
     setAudioState("stopped");
   };
 
+  // Handler to play the next channel
+  const handleNext = () => {
+    if (currentChannelIndex === channels.length - 1) {
+      setCurrentChannelIndex(0); // If at the last channel, loop back to the first channel
+    } else {
+      setCurrentChannelIndex(currentChannelIndex + 1); // Move to the next channel
+    }
+    setAudioState("stopped"); // Stop playback when changing channels
+  };
+
+  // Handler to play the previous channel
+  const handlePrevious = () => {
+    if (currentChannelIndex === 0) {
+      setCurrentChannelIndex(channels.length - 1); // If at the first channel, move to the last channel
+    } else {
+      setCurrentChannelIndex(currentChannelIndex - 1); // Move to the previous channel
+    }
+    setAudioState("stopped"); // Stop playback when changing channels
+  };
+
   if (loading) return <p>Loading...</p>;
 
   return (
     <div className="player-container">
       {/* Image */}
       <div className="img-container">
-        <img src={channels[0].imagetemplate} alt="Album Art" />
+        <img
+          src={channels[currentChannelIndex].imagetemplate}
+          alt="Album Art"
+        />
       </div>
       {/* Channel Name */}
-      <h3 className="channel-name">{channels[0].name}</h3>
+      <h3 className="channel-name">{channels[currentChannelIndex].name}</h3>
       {/* Audio Player */}
       <audio
         ref={audioRef}
-        src={channels[0].liveaudio.url}
+        src={channels[currentChannelIndex].liveaudio.url}
         autoPlay={false}
         controls
       ></audio>
       {/* Controls */}
       <div className="player-controls">
+        {/* Previous Button */}
+        <FaBackward
+          className="control-icon"
+          onClick={handlePrevious}
+          title="Previous"
+        />
         {/* Play Button */}
         {audioState === "stopped" && (
           <FaPlay className="control-icon" onClick={handlePlay} title="Play" />
@@ -49,6 +79,8 @@ const Player = () => {
         {audioState === "playing" && (
           <FaStop className="control-icon" onClick={handleStop} title="Stop" />
         )}
+        {/* Next Button */}
+        <FaForward className="control-icon" onClick={handleNext} title="Next" />
       </div>
     </div>
   );
